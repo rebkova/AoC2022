7c740d43118d54c91ed99ee23b2ed28ffbae9747
rebkova <rebeka.kovacic@hypergene.se>
2022-12-18 21:23:35 +0100

    Day 04 solution part 1

--- Day04/day_04_1.js
+++ Day04/day_04_1.js
@@ -2,31 +2,25 @@ const fs = require("fs");
 const data = fs.readFileSync("data_day04.txt", 'utf8');
 
 let dataAsArray = data.toString().split(/\r?\n/);
-console.log('dataAsArray:', dataAsArray);
 // dataAsArray: [ '2-4,6-8', '2-3,4-5', '5-7,7-9', '2-8,3-7', '6-6,4-6', '2-6,4-8' ]
 
 let dataInPairs = [];
 for (d = 0; d < dataAsArray.length; d++) {
-    dataInPairs.push(dataAsArray[d].split(','));
+    // split at non-digits only
+    dataInPairs.push(dataAsArray[d].split(/\D/));
 }
-console.log('dataInPairs:', dataInPairs);
 
 let arrOfFullContains = [];
-for (i = 0; i < dataAsArray.length; i++) {
-    const pairOfElves = dataAsArray[i];
-    console.log('pairOfElves:', pairOfElves);
-    if (Number(pairOfElves[0]) <= Number(pairOfElves[4]) && Number(pairOfElves[2]) >= Number(pairOfElves[6])) {
-        // console.log('pairOfElves[0]:', pairOfElves[0]);
-        console.log('first if');
+for (i = 0; i < dataInPairs.length; i++) {
+    const pairOfElves = dataInPairs[i];
+
+    if (Number(pairOfElves[0]) <= Number(pairOfElves[2]) && Number(pairOfElves[1]) >= Number(pairOfElves[3])) {
         arrOfFullContains.push(1);
-        console.log('arrOfFullContains first:', arrOfFullContains);
-    } else if (Number(pairOfElves[4]) <= Number(pairOfElves[0]) && Number(pairOfElves[6]) >= Number(pairOfElves[2])) {
-        console.log('second if');
+    } else if (Number(pairOfElves[2]) <= Number(pairOfElves[0]) && Number(pairOfElves[3]) >= Number(pairOfElves[1])) {
         arrOfFullContains.push(1);
-        console.log('arrOfFullContains second:', arrOfFullContains);
     }
-    console.log('arrOfFullContains at the end of for loop:', arrOfFullContains);
 }
-console.log('arrOfFullContains:', arrOfFullContains);
+
 const sumOfContains = arrOfFullContains.reduce((a, b) =>  a + b, 0);
 console.log('sumOfContains:', sumOfContains);
+// solution part 1: 644
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ TILs:
 - readFileSync inbuilt node module can be used for synchronous reading of data
 - .codeCharAt() method on string returns a decimal value of a character according to ASCII table
 - do not forget to assign the incremented value! (: (i=+3 instead of i+3)
+--
+:star: Day04:
+- .split() will take the regex and split at the matching spot! i.e. if we only want digits returned, regex should match all non-digits
